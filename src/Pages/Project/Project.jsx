665529a9152fbdd710d1c8549a03bb6762388809
Akash Rahman <mraakash01932@gmail.com>
2022-11-24 01:29:32 +0600

    firebase problem solved

--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,31 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
 
 const Project = () => {
-  const handleSubmit=(e)=>{
-    e.preventDefault( )
-    const image = e.target.img.files[0]
-    const data = new FormData()
-    data.append("file",image)
-    data.append("upload_preset","NJ_images")
-    data.append("cloud_name","dvmwear6h")
-    fetch("https://api.cloudinary.com/v1_1/dvmwear6h/image/upload",{
-      method:"POST",
-      body:data
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch((err)=>{
-      console.log(err);
-    })
-  }
+ 
   return (
     <div>
       <h1>projects</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="file" name='img'/>
-        <button type='submit'>Submit</button>
-      </form>
+      
     </div>
   )
 }
